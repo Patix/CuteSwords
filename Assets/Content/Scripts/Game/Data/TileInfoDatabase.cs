b39d81212a1d711e8b93d6ed25f7e5b8019e4c03
Patiko <patik.fetter@gmail.com>
2024-10-14 02:58:01 +0400

    Basic Tile Database

--- Assets/Content/Scripts/Game/Data/TileInfoDatabase.cs
+++ Assets/Content/Scripts/Game/Data/TileInfoDatabase.cs
@@ -1,19 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Patik.CodeArchitecture.Patterns;
 using Sirenix.OdinInspector;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class TileInfoDatabase : SingletonSerializedScriptableObject <TileInfoDatabase>
 {
-    [SerializeField, Searchable] private Dictionary <Sprite, int>       m_Elevations     = new();
-    [SerializeField, Searchable] private Dictionary <Sprite, Vector3>   m_Rotations      = new();
-    [SerializeField, Searchable] private Dictionary <Sprite, AudioClip> m_FootstepSounds = new();
+    [SerializeField, Searchable] private Dictionary <Sprite, int>        m_Elevations     = new();
+    [SerializeField, Searchable] private Dictionary <Sprite, Vector3>    m_Rotations      = new();
+    [SerializeField, Searchable] private Dictionary <Sprite, AudioClip>  m_FootstepSounds = new();
+    [SerializeField, Searchable] private Dictionary <Sprite, GameObject> m_SurfacePrefabs = new();
 
-    public static AudioClip GetFootAudio(Sprite terrainSprite) { return Instance.m_FootstepSounds[terrainSprite]; }
+    public static AudioClip GetFootAudio(Sprite    terrainSprite)                     { return Instance.m_FootstepSounds[terrainSprite]; }
+    public static bool      TryGetRotation(Sprite  terrainSprite, out Vector3 result) { return Instance.m_Rotations.TryGetValue(terrainSprite, out result);}
+    public static bool      TryGetElevation(Sprite terrainSprite, out int elevation) { return Instance.m_Elevations.TryGetValue(terrainSprite, out elevation);}
 
-
+    public static GameObject GetSurfacePrefab(Sprite sprite)
+    {
+        return Instance.m_SurfacePrefabs[sprite];
+    }
+    
     [Button, BoxGroup("Buttons")]
     void AssignAudio(Sprite[] sprites , AudioClip audioClip)
     {
@@ -24,6 +34,18 @@ public class TileInfoDatabase : SingletonSerializedScriptableObject <TileInfoDat
                 m_FootstepSounds[sprite] = audioClip;
             }
         }
+    }
+
+    [Button]
+    void AddAllSprites(Sprite[] sprites , GameObject []prefabs)
+    {
+        foreach (Sprite sprite in sprites)
+        {
+            var correctPrefab = prefabs.FirstOrDefault(x => sprite.name.Contains(x.name.Split("_").Last(), StringComparison.InvariantCultureIgnoreCase));
+
+            m_SurfacePrefabs.TryAdd(sprite, correctPrefab);
+        }
+     
     }
 }
     
